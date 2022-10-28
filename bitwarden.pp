b0704fb8bfb54e82171e880a3b72a39ff5fa20e6
lea <blue.blood@gmx.de>
2022-10-28 20:13:53 +0200

    Docker dependency and setup

--- bitwarden.pp
+++ bitwarden.pp
@@ -1,18 +1,31 @@
 node default {
-        group { 'docker':
-                ensure => 'present',
-                name => 'docker',
+        class { 'docker':
+                version => latest,
+                docker_users => ['bitwarden'],
         }
+
+        docker::image { 'vaultwarden/server':
+                image_tag => 'latest'
+        }
+
+        docker::run { 'vaultwarden':
+                image => 'vaultwarden/server',
+                detach => true,
+                volumes => ['/opt/bitwarden'],
+                ports => ['80'],
+                expose => ['80'],
+                command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
+        }
+
         file { '/opt/bitwarden':
                 ensure => 'directory',
                 mode => '0700',
         }
+
         user { 'bitwarden':
                 ensure => 'present',
                 name => 'bitwarden',
-                groups => 'docker',
                 # password => 'hereisaplaceforoursupersensitivepassword',
                 home => '/opt/bitwarden',
         }
 }
-
