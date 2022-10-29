698e5015c9c36c44bc98dc25843a41889830fa54
lea <blue.blood@gmx.de>
2022-10-29 19:02:04 +0200

    Try to follow the approach of docker.pp (still not working though)

--- bitwarden.pp
+++ bitwarden.pp
@@ -1,24 +1,25 @@
-node default {
-        class { 'docker':
-                version => latest,
-        }
+class { 'docker':
+        version => latest,
+        docker_users => ['ubuntu'],
+}
+
+docker_volume { 'vaultwarden':
+        ensure => present,
+}
+
+docker::image { 'vaultwarden/server':
+        image_tag => 'latest',
+        ensure => present
+}
 
 
-        docker_volume { 'vaultwarden':
-                ensure => present,
-        }
-
-        docker::image { 'vaultwarden/server':
-                image_tag => 'latest',
-                ensure => present
-        }
-
-        docker::run { 'vaultwarden':
-                image => 'vaultwarden/server',
-                detach => true,
-                volumes => ['vaultwarden:/data'],
-                ports => ['80:80'],
-                restart_service => true
-        }
+docker::run { 'vaultwarden':
+        image  => 'vaultwarden/server',
+        ports  => ['80:80'],
+        detach => true,
+        volumes => ['vaultwarden:/data'],
+        restart_service => true,
+        require => Class['docker'],
+        ensure => present
 }
 
